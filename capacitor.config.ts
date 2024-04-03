fef8f300200e75277c8387ba684d794ec983b26b
Israel Gutierrez <igutierrez@live.cl>
2024-04-02 23:17:28 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'HotelPacificReef-Ionic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
